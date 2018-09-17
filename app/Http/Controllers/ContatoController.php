2d324ebc080a67e74144410fb2ab93ee11702007
Vicente Silva <vicente.pspenha@gmail.com>
2018-09-17 12:27:10 -0300

    [mod] created functions save() and findAll()

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,19 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Contato;
 
 class ContatoController extends Controller {
+
+    protected $contato;
     
-    public function save($contato){
-        return $contato;
-//        $teste = ['Teste 1', 'Teste 2'];
-//        return $teste;
+    public function save(Request $request){        
+        if ($request->isMethod('get')){
+            $data = $request->toArray();
+            $contato = new Contato();
+            
+            $contato->nome = mb_strtoupper($data['nome'], 'utf-8');
+            $contato->telefone = $data['telefone'];
+            $contato->email = mb_strtoupper($data['email'], 'utf-8');
+            
+            $contato->save();
+            return response()->json(['response' => 'Salvo com sucesso!']);
+        }
+        return response()->json(['response' => 'erro ao salvar']);
     }
     
-    public function find($id){
-        $result = \App\Contato::all();
-        var_dump($result);die;
+    public function findAll(Request $request){
+        $result = Contato::all()->toArray();
         return $result;
-    }
+    }   
     
 }
