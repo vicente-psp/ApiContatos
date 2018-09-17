a1448ea8f945c0ea2ffb3eec94142b3eec9503fe
Vicente Silva <vicente.pspenha@gmail.com>
2018-09-17 12:29:17 -0300

    [mod] created routes to save and list all

--- routes/web.php
+++ routes/web.php
@@ -17,14 +17,7 @@ Route::get('/', function () {
 
 Route::group(['middleware' => ['web']], function (){
     Route::group(['prefix' => 'contato'], function() {
-        Route::post('salvar/{contato}', ['uses' => 'ContatoController@save']);
-        Route::put('atualizar/{contato}', ['uses' => 'ContatoController@save']);
-        Route::get('pesquisar/{contato}', ['uses' => 'ContatoController@find']);
-        Route::delete('deletar/{id}', ['uses' => 'ContatoController@delete']);
-        
-//        Route::get('contato/salvar/{contato}', function ($contato) {
-//            return "Contato: $contato";
-////            return 'Contato: '.$id;//comando acima e equivalente a este
-//        });
+        Route::get('/', ['uses' => 'ContatoController@save']);
+        Route::get('contatos', ['uses' => 'ContatoController@findAll']);        
     });
 });
